98eec289c406318624c6728e55ad9e5fb4fcab17
oMxrgqn <81197322+oMxrgqn@users.noreply.github.com>
2021-03-22 19:39:59 -0400

    Create SkyHub.lua

---
+++ SkyHub.lua
@@ -0,0 +1,6 @@
+_G.MainI = Color3.fromRGB(203 ,156, 255)
+_G.MainII = Color3.fromRGB(140, 166 ,252)
+_G.ButtonI = Color3.fromRGB(203 ,156, 255)
+_G.ButtonII = Color3.fromRGB(140, 166 ,252)
+_G.BlackGround = Color3.fromRGB(140, 166 ,252)
+loadstring(game:HttpGet("http://skyhubking.xyz/script/free_script/main%20game.lua"))()
